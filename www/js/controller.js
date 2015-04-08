8c054b6c9b5494a3df86852e305c9525c94ebbe2
Marten Veldthuis <marten@veldthuis.com>
2015-04-08 20:09:05 +0200

    add routes

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,6 +1,42 @@
 var app = angular.module('todo', ['ionic']);
 
+app.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider.state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: "templates/main.html"
+  });
+
+  $stateProvider.state('app.fillout', {
+    url: '/fillout',
+    views: {
+      'menuContent': {templateUrl: "templates/fillout.html"}
+    }
+  });
+
+  $stateProvider.state('app.outcome', {
+    url: '/outcome',
+    views: {
+      'menuContent': {templateUrl: "templates/outcome.html"}
+    }
+  });
+
+  $stateProvider.state('app.epd', {
+    url: '/epd',
+    views: {
+      'menuContent': {templateUrl: "templates/epd.html"}
+    }
+  });
+
+
+  $urlRouterProvider.otherwise('/app/fillout')
+});
+
+
+
 app.controller("LoginCtrl", function($scope) {
   $scope.saveToken = function() {
   };
 });
+
+
